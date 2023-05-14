2b5b6df765705c2c1bbc9a9b83beb75c57f5f460
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-14 20:01:54 +0300

    The plugin installer class

---
+++ src/Installer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The plugin installer class.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.0.0
+ *
+ * @package woo-paddle-gateway
+ */
+
+namespace Woo_Paddle_Gateway;
+
+/**
+ * The plugin installer class.
+ */
+class Installer {
+
+	/**
+	 * Activate the plugin.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public static function activate(): void {}
+
+	/**
+	 * Deactivate the plugin.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public static function deactivate(): void {}
+}
