4f69118fb81bb63ee38a03133cf58c263337ff9e
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-14 18:59:13 +0300

    Bootstrap the plugin

---
+++ woo-paddle-gateway.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The `Paddle Payment Gateway for WooCommerce` bootstrap file.
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * You can redistribute this plugin/software and/or modify it under
+ * the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * @link https://mypreview.one
+ *
+ * @since 1.0.0
+ *
+ * @package woo-paddle-gateway
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @copyright © 2015 - 2023 MyPreview. All Rights Reserved.
+ *
+ * @wordpress-plugin
+ * Plugin Name:          Woo Paddle Gateway
+ * Plugin URI:           https://mypreview.one
+ * Description:          Accept Visa, MasterCard, American Express, Discover, JCB, Diners Club, iDEAL, Alipay, and more directly on your store with the Paddle payment gateway for WooCommerce, including PayPal, Apple Pay, Google Pay, and Microsoft Pay for mobile and desktop.
+ * Version:              1.0.0
+ * Author:               MyPreview
+ * Author URI:           https://mypreview.one
+ * Requires at least:    6.0
+ * Requires PHP:         7.4
+ * License:              GPL-3.0
+ * License URI:          http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:          woo-paddle-gateway
+ * Domain Path:          /languages
+ * WC requires at least: 7.0
+ * WC tested up to:      7.7
+ */
+
+namespace Woo_Paddle_Gateway;
