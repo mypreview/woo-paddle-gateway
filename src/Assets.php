2ea5970e8f3484e4b38669640293efd5a56f83a6
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-05-15 00:19:30 +0300

    Code tidy

--- src/Assets.php
+++ src/Assets.php
@@ -34,24 +34,19 @@ abstract class Assets {
 	 */
 	public static function enqueue_admin(): void {
 
-		wp_register_style( 'woo-paddle-gateway', self::get_asset_path( 'admin.css' ), array(), woo_paddle_gateway()->get_version(), 'screen' );
-		wp_register_script( 'woo-paddle-gateway', self::get_asset_path( 'admin.js' ), array( 'jquery' ), woo_paddle_gateway()->get_version(), true );
-	}
-
-	/**
-	 * Returns a full path for the asset (static resource CSS/JS) file.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param string $file_name Asset file name (filename).
-	 *
-	 * @return string
-	 */
-	private static function get_asset_path( string $file_name ): string {
-
-		$min       = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : trailingslashit( 'minified' );
-		$directory = pathinfo( $file_name, PATHINFO_EXTENSION );
-
-		return woo_paddle_gateway()->service( 'file' )->plugin_url( "assets/{$directory}/{$min}{$file_name}" );
+		wp_register_style(
+			'woo-paddle-gateway',
+			woo_paddle_gateway()->service( 'file' )->asset_path( 'admin.css' ),
+			array(),
+			woo_paddle_gateway()->get_version(),
+			'screen'
+		);
+		wp_register_script(
+			'woo-paddle-gateway',
+			woo_paddle_gateway()->service( 'file' )->asset_path( 'admin.js' ),
+			array( 'jquery' ),
+			woo_paddle_gateway()->get_version(),
+			true
+		);
 	}
 }
