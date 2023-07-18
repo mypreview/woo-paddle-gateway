1e0337249af91d0fe75a287ff22f03aaed708da7
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-07-18 22:48:23 +0300

    The plugin template manager class

---
+++ src/TemplateManager.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The plugin template manager class.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.0.0
+ *
+ * @package woo-paddle-gateway
+ */
+
+namespace Woo_Paddle_Gateway;
+
+/**
+ * Class TemplateManager.
+ */
+class TemplateManager {
+
+	/**
+	 * Render the template.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $template_name The template name.
+	 * @param array  $args          The template arguments.
+	 *
+	 * @return void
+	 */
+	public function echo_template( $template_name, $args = array() ) {
+
+		// Supports internal WooCommerce caching.
+		wc_get_template(
+			$template_name,
+			$args,
+			'',
+			trailingslashit( woo_paddle_gateway()->service( 'file' )->plugin_path( 'templates' ) )
+		);
+	}
+}
