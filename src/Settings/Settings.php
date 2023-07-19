bbebb1117a2f024b590916664b6c3bbb100ea967
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-07-19 12:44:29 +0300

    Process the gateway settings

--- src/Settings/Settings.php
+++ src/Settings/Settings.php
@@ -65,6 +65,7 @@ class Settings extends WC_Payment_Gateway {
 	public function setup() {
 
 		add_filter( 'admin_body_class', array( $this, 'add_body_class' ) );
+		add_action( "woocommerce_update_options_payment_gateways_{$this->id}", array( $this, 'process_admin_options' ) );
 	}
 
 	/**
@@ -86,6 +87,43 @@ class Settings extends WC_Payment_Gateway {
 		$classes .= sprintf( ' %s-page', sanitize_html_class( $this->id ) );
 
 		return $classes;
+	}
+
+	/**
+	 * Process the gateway settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function process_admin_options(): void {
+
+		// Save the gateway settings.
+		parent::process_admin_options();
+
+		$data       = $this->get_post_data();
+		$is_sandbox = wc_string_to_bool( $this->get_field_value( 'sandbox_mode', 'checkbox', $data ) );
+		$mode       = $is_sandbox ? 'test' : 'live';
+		$verify     = $this->verify_connection_status( $data, $is_sandbox );
+
+		// Bail early in case the API connection status is not set.
+		if ( ! $verify ) {
+			return;
+		}
+
+		// Update the API connection status.
+		$this->update_option( "{$mode}_vendor_verify", wc_bool_to_string( $verify ) );
+
+		/**
+		 * Fires after the gateway settings are saved.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array  $data   The gateway settings data.
+		 * @param string $mode   The gateway mode (test/live).
+		 * @param bool   $verify The API connection status.
+		 */
+		do_action( 'woo_paddle_gateway_settings_saved', $data, $mode, $verify );
 	}
 
 	/**
@@ -117,5 +155,54 @@ class Settings extends WC_Payment_Gateway {
 	public function init_form_fields() {
 
 		$this->form_fields = woo_paddle_gateway()->service( 'settings_general' )->get_fields();
+	}
+
+	/**
+	 * Verify the API connection status.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string|array $data       The POST data.
+	 * @param bool         $is_sandbox Whether the gateway is in sandbox mode or not.
+	 *
+	 * @return null|string
+	 */
+	private function verify_connection_status( $data, $is_sandbox ) {
+
+		$mode             = $is_sandbox ? 'test' : 'live';
+		$saved_keys       = woo_paddle_gateway()->service( 'gateway' )->get_keys();
+		$vendor_id        = $this->get_field_value( "{$mode}_vendor_id", 'text', $data );
+		$vendor_auth_code = $this->get_field_value( "{$mode}_vendor_auth_code", 'text', $data );
+
+		// Bail early in case the API credentials have not changed.
+		if ( $saved_keys['vendor_id'] === $vendor_id && $saved_keys['vendor_auth_code'] === $vendor_auth_code ) {
+			return null;
+		}
+
+		$request = wp_remote_post(
+			woo_paddle_gateway()->service( 'endpoints' )->get( 'public_key', $is_sandbox ),
+			array(
+				'timeout' => 30,
+				'body'    => array(
+					'vendor_id'        => wc_clean( $vendor_id ),
+					'vendor_auth_code' => wc_clean( $vendor_auth_code ),
+				),
+			)
+		);
+
+		// Check if the request was successful.
+		if ( is_wp_error( $request ) || 200 !== wp_remote_retrieve_response_code( $request ) ) {
+			return false;
+		}
+
+		// Decode the response body.
+		$response = json_decode( wp_remote_retrieve_body( $request ) );
+
+		// Check if the response is valid.
+		if ( ! empty( $response->success ) && ! empty( $response->response->public_key ) ) {
+			return false;
+		}
+
+		return true;
 	}
 }
